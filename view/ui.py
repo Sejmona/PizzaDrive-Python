06e726cf325feb422fd9b9191229eb679aac54be
Simona Šmelhausová <101262454+Sejmona@users.noreply.github.com>
2024-08-22 10:30:53 +0200

    add ui.py

---
+++ view/ui.py
@@ -0,0 +1,10 @@
+class UI:
+    def display_main_menu(self):
+        print("1. Create Order")
+        print("2. Pay")
+        print("3. Admin Menu")
+        print("4. Exit")
+
+    def display_order_menu(self):
+        print("1. Pizza from Menu")
+        print("2. Custom Pizza")
